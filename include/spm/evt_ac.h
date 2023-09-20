43fcf2347996f94f39578a97548e1f7ca18996f0
L <dr.trayaurus61@gmail.com>
2023-09-20 11:14:40 -0700

    fix CI, add lst values for acdrv, change name of evt_ac_name_to_ptr

--- include/spm/evt_ac.h
+++ include/spm/evt_ac.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <common.h>
+#include <evt_cmd.h>
 
 CPP_WRAPPER(spm::evt_ac)
 
 // evt_ac_entry(const char * name, s32 type)
 EVT_DECLARE_USER_FUNC(evt_ac_entry, 2);
 
-//evt_ac_name_to_ptr(const char * name, EvtVar storage)
-EVT_DECLARE_USER_FUNC(evt_ac_name_to_ptr, 2)
+//evt_ac_return_results(const char * name, s32& ret)
+EVT_DECLARE_USER_FUNC(evt_ac_return_results, 2)
 
 //evt_ac_delete(const char * name)
 EVT_DECLARE_USER_FUNC(evt_ac_delete, 1)
