d5a3bae1f4447be0b2ae0deda77fa9e35684d2b2
L <dr.trayaurus61@gmail.com>
2024-08-01 15:13:26 -0700

    fixed spm headers

---
+++ include/spm/npc_dimeen_l.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <common.h>
+#include <spm/npcdrv.h>
+
+CPP_WRAPPER(spm::npc_dimeen_l)
+
+
+extern spm::npcdrv::NPCDefense superDimentioHeadDefenses;
+extern spm::npcdrv::NPCDefense superDimentioBodyDefenses;
+
+CPP_WRAPPER_END()
