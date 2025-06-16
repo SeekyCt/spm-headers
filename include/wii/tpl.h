0f113867a998a3f4017cde55502365d025f92c96
Lily Lawliet <dr.trayaurus61@gmail.com>
2025-06-16 15:27:26 -0700

    remove unnecessary wii semicolons

--- include/wii/tpl.h
+++ include/wii/tpl.h
@@ -109,7 +109,7 @@ SIZE_ASSERT(TPLHeader, 0xc)
 */
 void TPLBind(TPLHeader * palette);
 
-UNKNOWN_FUNCTION(TPLGet);
+UNKNOWN_FUNCTION(TPLGet)
 void TPLGetGXTexObjFromPalette(TPLHeader * palette, GXTexObj * dest, u32 id);
 
 CPP_WRAPPER_END()
