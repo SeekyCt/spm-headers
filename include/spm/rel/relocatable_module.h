227f1fb12caea0f2d1c52de7b89230ccdb301641
SeekyCt <58006653+SeekyCt@users.noreply.github.com>
2026-01-08 00:58:35 +0000

    Don't expose _prolog to mods for now

--- include/spm/rel/relocatable_module.h
+++ include/spm/rel/relocatable_module.h
@@ -4,7 +4,11 @@
 
 CPP_WRAPPER(spm::relocatable_module)
 
-s32 _prolog();
+// Mods define functions with the same names
+// TODO: some kind of prefix to allow patching the game's
+#ifdef DECOMP
+void _prolog();
 // more
+#endif
 
 CPP_WRAPPER_END()
